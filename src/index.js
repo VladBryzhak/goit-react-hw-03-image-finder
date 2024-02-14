41ac8920d6ddf3539a89b43940cbe96819f22ab0
VladBryzhak <124492057+VladBryzhak@users.noreply.github.com>
2024-02-14 21:17:49 +0200

    finale

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+
+const theme = {
+  colors: {
+    black: '#000000',
+    error: 'red',
+  },
+  radius: '5px',
+  spasing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <GlobalStyle/>
+    </ThemeProvider>
   </React.StrictMode>
 );
